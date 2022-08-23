2b67bbb9b8cbfa5a770004d6503e6e5ecacf5325
SurajiddinShomurotov1609 <shomuratovsurajiddin@gmail.com>
2022-08-23 11:19:44 +0300

    prices qo'shildi

--- nav.php
+++ nav.php
@@ -7,7 +7,7 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="customers/index.php">Navbat</a>
+                <a class="nav-link active" aria-current="page" href="/avtoservis/customers/index.php">Navbat</a>
                 <a class="nav-link" href="#">Features</a>
                 <a class="nav-link" href="#">Pricing</a>
                 <a class="nav-link disabled">Disabled</a>
--- prices/index.php
+++ prices/index.php
@@ -1,3 +1,12 @@
+<?php
+
+include "../nav.php";
+require_once "../users/connection.php";
+$sql = "select * from prices";
+$result = mysqli_query($connection,$sql);
+
+?>
+
 <!doctype html>
 <html lang="en">
 <head>
@@ -7,8 +16,40 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
 </head>
 <body>
-<?php include "../nav.php"?>
-<h1>Hello, world!</h1>
+
+
+<div class="card">
+    <h1 align="center" class="text text-primary">Narxlar ro'yhati</h1>
+</div>
+<br>
+<div class="container w-50">
+    <table class="table table-bordered boredr-1">
+        <tr>
+            <th>#</th>
+            <th>Ism</th>
+            <th>Narxi</th>
+        </tr>
+
+        <?php
+        if(mysqli_num_rows($result)>0){
+            while ($row=mysqli_fetch_assoc($result)){
+
+        ?>
+
+        <tr>
+            <td> <?php echo $row['id'] ?> </td>
+            <td> <?php echo $row['name'] ?> </td>
+            <td> <?php echo $row['price'] ?> </td>
+        </tr>
+
+        <?php
+            }
+        }
+        ?>
+
+    </table>
+</div>
+
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-A3rJD856KowSb7dwlZdYEkO39Gagi7vIsF0jrRAoQmDKKtQBHUuLZ9AsSv4jD4Xa" crossorigin="anonymous"></script>
 </body>
 </html>
