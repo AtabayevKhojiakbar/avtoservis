a946793da7ac988f9daa49d87207d0007329367e
Hojiakbar <hatabayev17@gmail.com>
2022-08-23 17:52:10 +0500

    nav.php to`g`irlandi

--- nav.php
+++ nav.php
@@ -7,9 +7,9 @@
         </button>
         <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div class="navbar-nav">
-                <a class="nav-link active" aria-current="page" href="customers/index.php">Navbat</a>
+                <a class="nav-link active" aria-current="page" href="/avtoservis/customers/index.php">Navbat</a>
                 <a class="nav-link" href="#">Features</a>
-                <a class="nav-link" href="prices/index.php">Narxlar</a>
+                <a class="nav-link" href="/avtoservis/prices/index.php">Narxlar</a>
                 <a class="nav-link disabled">Disabled</a>
             </div>
         </div>
