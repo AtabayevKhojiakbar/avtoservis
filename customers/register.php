bedcadefdaf0dd7896bd294642251e7e1a1e1074
Hojiakbar <hatabayev17@gmail.com>
2022-08-24 19:06:27 +0500

    customers/register.php update

--- customers/register.php
+++ customers/register.php
@@ -1,6 +1,6 @@
 <?php
     include "../users/connection.php";
-    $bool = false;
+
     if(isset($_POST['submit'])){
         $car=$_POST['car_number'];
         $employee = $_POST['employee'];
@@ -13,19 +13,15 @@
         $prce = mysqli_fetch_assoc($prc);
         $id_prc = $prce['id'];
         $prc_sum = $prce['price'];
-        $payed = 0;
         if($id_emp%2==0) {
             $sale = 10;
-
         }
         else {
             $sale = 5;
-
         }
-
+        $payed = $prc_sum*(100-$sale)/100;
         $sql = "insert into customers(car_number, employee_id, price_id, paysum, sale, payedsum) values ('$car', '$id_emp', '$id_prc', '$prc_sum', '$sale', '$payed');";
         $natija = mysqli_query($connection, $sql);
-        if($natija) $bool = true;
     }
 
     header('location:index.php');
